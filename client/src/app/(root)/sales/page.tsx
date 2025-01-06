e417bdb3ee59609508739f4a5e9a98256148c5d9
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2025-01-07 00:28:43 +0530

    Initial Commit

---
+++ client/src/app/(root)/sales/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function page() {
+    return (
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <div className="flex rounded-xl bg-muted/50 p-6 justify-end gap-10">
+                <Button className="font-bold">Create Invoice</Button>
+                <Button className="font-bold">Create Estimate</Button>
+            </div>
+            <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        </div>
+    )
+}
