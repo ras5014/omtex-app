9f49054f4e331e54fd6c415b0613a60f19c2ae8f
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2025-08-16 02:10:27 +0530

    fix(axios): update baseURL environment variable to NEXT_PUBLIC_STRAPI_BASE_URL

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.STRAPI_BASE_URL || "http://localhost:1337/api",
+  baseURL:
+    process.env.NEXT_PUBLIC_STRAPI_BASE_URL || "http://localhost:1337/api",
   headers: {
     // Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
     "Content-Type": "application/json",
