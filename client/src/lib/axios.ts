8f941bbd44f3f10ff2fce85f929a536ba1075e6c
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2025-08-16 01:58:37 +0530

    fix(axios): correct baseURL environment variable name

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.STRAPI_URL || "http://localhost:1337/api",
+  baseURL: process.env.STRAPI_BASE_URL || "http://localhost:1337/api",
   headers: {
     // Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
     "Content-Type": "application/json",
